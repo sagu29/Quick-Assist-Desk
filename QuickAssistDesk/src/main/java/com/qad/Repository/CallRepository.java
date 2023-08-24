05453015f884262a8b04861d9a600fa112258486
Sagar Nikhare <121310880+sagu29@users.noreply.github.com>
2023-08-24 22:48:03 +0530

    Delete QuickAssistDesk/src/main/java/com/qad directory

--- QuickAssistDesk/src/main/java/com/qad/Repository/CallRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qad.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qad.Entity.Call;
-
-public interface CallRepository extends JpaRepository<Call, Integer>{
-
-}
