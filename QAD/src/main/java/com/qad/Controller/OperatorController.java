cbac82d244d984db77bae6fa6e9286f0a00c01c6
sagu29 <sagarnikhare1997@gmail.com>
2023-10-12 12:18:40 +0530

    java documentation and comments to the all methods of customer controller class and operator controller class

--- QAD/src/main/java/com/qad/Controller/OperatorController.java
+++ QAD/src/main/java/com/qad/Controller/OperatorController.java
@@ -24,89 +24,132 @@ import com.qad.Service.OperatorService;
 
 import jakarta.validation.Valid;
 
+/**
+ * Controller class for handling operator-related operations.
+ */
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/operator")
 public class OperatorController {
-	
-	@Autowired
-	private OperatorService operatorService ;
-	
-	
-	
-	@PostMapping("/operatorLogin")
-	public ResponseEntity<CurrentOperatorSession> loginOperator(@RequestBody Login login) throws LoginException, OperatorException {
-		
-		CurrentOperatorSession result = operatorService.loginOperator(login);
-			
-		return new ResponseEntity<CurrentOperatorSession>(result,HttpStatus.OK );
-	}
-	
-	
-	
-	@PostMapping("/addIssue")
-	public ResponseEntity<Issue> addCustomerIssueHandler(@RequestBody @Valid Issue issue) throws OperatorException{
-		
-		Issue i = operatorService.addCustomerIssue(issue);
-		
-		return new ResponseEntity<Issue>(i,HttpStatus.CREATED);
-	}
-	
-	
-	
-	@PutMapping("/modifyIssue/{issueId}")
-	public ResponseEntity<Issue> modifyIssueHandler(@RequestBody @Valid Issue issue, @PathVariable Integer issueId)throws OperatorException {
-		
-		Issue i = operatorService.modifyCustomerIssue(issue, issueId);
-		
-		return new ResponseEntity<Issue>(i,HttpStatus.OK);
-	}
-	
-	
-	
-	@PutMapping("/close/{issueId}")
-    public ResponseEntity<String> closeCustomerIssue(@RequestBody Issue issue, @PathVariable Integer issueId) throws OperatorException {
-        
-            operatorService.closeCustomerIssue(issue, issueId);
-            
-            return ResponseEntity.status(HttpStatus.OK).body("Issue closed successfully");
-       
+
+    @Autowired
+    private OperatorService operatorService;
+
+    /**
+     * Log in an operator using the provided login credentials.
+     *
+     * @param login The login credentials, including a username and password.
+     * @return The current operator session if the login is successful.
+     * @throws LoginException If there's an issue related to the operator account.
+     * @throws OperatorException If there's an issue with the login process.
+     */
+    @PostMapping("/operatorLogin")
+    public ResponseEntity<CurrentOperatorSession> loginOperator(@RequestBody Login login) throws LoginException, OperatorException {
+
+        CurrentOperatorSession result = operatorService.loginOperator(login);
+
+        return new ResponseEntity<CurrentOperatorSession>(result, HttpStatus.OK);
     }
 
-	
-	
-	
-	@GetMapping("/customerid/{customerId}")
-	public ResponseEntity<Customer> getCustomerByIdHandler(@PathVariable @Valid Integer customerId)throws OperatorException {
-		
-		Customer c = operatorService.findCustomerById(customerId);
-		
-		return new ResponseEntity<Customer>(c,HttpStatus.OK);
-	}
-	
-	
-	
-	@GetMapping("/customername/{name}")
-	public ResponseEntity<List<Customer>> getCustomerByFirstNameHandler(@PathVariable @Valid String name) throws OperatorException{
-		
-		List<Customer> cList =  operatorService.findCustomerByName(name);
-		
-		return new ResponseEntity<List<Customer>>(cList,HttpStatus.OK);
-	}
-	
-	
-	
-	@GetMapping("/customeremail/{email}")
-	public ResponseEntity<Customer> getCustomerByEmailHandler(@PathVariable @Valid String email)throws OperatorException {
-		
-		Customer c =  operatorService.findCustomerByEmail(email);
-		
-		return new ResponseEntity<Customer>(c,HttpStatus.OK);
-	}
-	@GetMapping("/AllIssue")
-	public ResponseEntity<List<Issue>> findAllIssue() throws OperatorException{
-		List<Issue> list=operatorService.findAllIssue();
-		return new ResponseEntity<List<Issue>>(list,HttpStatus.OK);
-	} 
-	
+    /**
+     * Add a new issue on behalf of a customer.
+     *
+     * @param issue The issue to be added.
+     * @return The added issue.
+     * @throws OperatorException If there's an issue with adding the issue.
+     */
+    @PostMapping("/addIssue")
+    public ResponseEntity<Issue> addCustomerIssueHandler(@RequestBody @Valid Issue issue) throws OperatorException {
+
+        Issue i = operatorService.addCustomerIssue(issue);
+
+        return new ResponseEntity<Issue>(i, HttpStatus.CREATED);
+    }
+
+    /**
+     * Modify an existing customer issue.
+     *
+     * @param issue    The modified issue information.
+     * @param issueId  The ID of the issue to be modified.
+     * @return The modified issue.
+     * @throws OperatorException If there's an issue with modifying the issue.
+     */
+    @PutMapping("/modifyIssue/{issueId}")
+    public ResponseEntity<Issue> modifyIssueHandler(@RequestBody @Valid Issue issue, @PathVariable Integer issueId) throws OperatorException {
+
+        Issue i = operatorService.modifyCustomerIssue(issue, issueId);
+
+        return new ResponseEntity<Issue>(i, HttpStatus.OK);
+    }
+
+    /**
+     * Close an existing customer issue.
+     *
+     * @param issue    The issue to be closed.
+     * @param issueId  The ID of the issue to be closed.
+     * @return A message indicating the success of closing the issue.
+     * @throws OperatorException If there's an issue with closing the issue.
+     */
+    @PutMapping("/close/{issueId}")
+    public ResponseEntity<String> closeCustomerIssue(@RequestBody Issue issue, @PathVariable Integer issueId) throws OperatorException {
+        operatorService.closeCustomerIssue(issue, issueId);
+        return ResponseEntity.status(HttpStatus.OK).body("Issue closed successfully");
+    }
+
+    /**
+     * Find a customer by their ID.
+     *
+     * @param customerId The ID of the customer to be found.
+     * @return The customer with the specified ID.
+     * @throws OperatorException If there's an issue with finding the customer.
+     */
+    @GetMapping("/customerid/{customerId}")
+    public ResponseEntity<Customer> getCustomerByIdHandler(@PathVariable @Valid Integer customerId) throws OperatorException {
+
+        Customer c = operatorService.findCustomerById(customerId);
+
+        return new ResponseEntity<Customer>(c, HttpStatus.OK);
+    }
+
+    /**
+     * Find customers by their first name.
+     *
+     * @param name The first name of customers to be found.
+     * @return A list of customers with the specified first name.
+     * @throws OperatorException If there's an issue with finding customers by name.
+     */
+    @GetMapping("/customername/{name}")
+    public ResponseEntity<List<Customer>> getCustomerByFirstNameHandler(@PathVariable @Valid String name) throws OperatorException {
+
+        List<Customer> cList = operatorService.findCustomerByName(name);
+
+        return new ResponseEntity<List<Customer>>(cList, HttpStatus.OK);
+    }
+
+    /**
+     * Find a customer by their email address.
+     *
+     * @param email The email address of the customer to be found.
+     * @return The customer with the specified email address.
+     * @throws OperatorException If there's an issue with finding the customer by email.
+     */
+    @GetMapping("/customeremail/{email}")
+    public ResponseEntity<Customer> getCustomerByEmailHandler(@PathVariable @Valid String email) throws OperatorException {
+
+        Customer c = operatorService.findCustomerByEmail(email);
+
+        return new ResponseEntity<Customer>(c, HttpStatus.OK);
+    }
+
+    /**
+     * Retrieve a list of all issues.
+     *
+     * @return A list of all issues in the system.
+     * @throws OperatorException If there's an issue with retrieving the list of issues.
+     */
+    @GetMapping("/AllIssue")
+    public ResponseEntity<List<Issue>> findAllIssue() throws OperatorException {
+        List<Issue> list = operatorService.findAllIssue();
+        return new ResponseEntity<List<Issue>>(list, HttpStatus.OK);
+    }
 }
