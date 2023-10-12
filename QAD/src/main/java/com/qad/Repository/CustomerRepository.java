c2857aa54096aa5c788a8984cf9e43bc8fa1c68f
sagu29 <sagarnikhare1997@gmail.com>
2023-10-12 12:31:15 +0530

    comments added in repository package for custom queries for all the methods

--- QAD/src/main/java/com/qad/Repository/CustomerRepository.java
+++ QAD/src/main/java/com/qad/Repository/CustomerRepository.java
@@ -10,11 +10,13 @@ import java.util.List;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
+	// Custom query method to find a customer by email
 	Optional<Customer> findByEmail(String email);
 	
+	 // Custom query method to find a customer by username
 	public Customer findByUsername(String userName);
-
+	
+	 // Custom query method to find customers by name
 	List<Customer> findByName(String name);
 	
 }
